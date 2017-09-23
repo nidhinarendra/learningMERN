c3b22e86162f3a6df54ef5e5db59b589df32bd6c
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-09-23 00:36:54 -0700

    Completed features to register, login and logout

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
+
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 
+//connect mongodb
+mongoose.connect(keys.mongoURI);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
+
+//to use cookies in our application
 
 //if there is no envirnoment variable defined in our development app then use 5000 (so it is 'or')
 const PORT = process.env.PORT || 5000;
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+//1 argument means we r fetching something from mongoose and 2 means we are inserting something
+//We need not specifically export and import here as it would create issues during testing(mocha or jmeter)
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +25,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a record of the profile id in the DB
+          done(null, existingUser);
+        } else {
+          //no record, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
